83c70e71da5476ec734b0ed51b35a6218cc79cf1
Андрей <andrey-otin@yandex.ru>
2020-07-13 22:39:18 +0300

    resize refactoring

--- src/Components/Table/resize.js
+++ src/Components/Table/resize.js
@@ -1,56 +1,57 @@
 import Table from '@/Components/Table/Table';
 
-let MoveHandler;
-let UpHandler;
+
+
 export const resizeHandler = (e, instance) => {
   const resizeEl = e.target.closest(Table.selectors.resizeEl);
   if (resizeEl) {
     e.preventDefault();
     const elementToRz = resizeEl.closest(Table.selectors.tableColumn);
     const startCoords = resizeEl.getBoundingClientRect();
-    instance.resize = {
+    let resizeData = {
       resizeEl,
       elementToRz,
       startCoords,
     };
-    MoveHandler = mouseMoveHandler.bind(null,instance);
-    UpHandler = mouseUpHandler.bind(null,instance);
-    instance.componentElement.addListener("mousemove", MoveHandler);
-    instance.componentElement.addListener("mouseup", UpHandler);
+
+  
+    const mouseMoveHandler = (e) => {
+      e.preventDefault();
+      if (resizeData.resizeEl.className.includes("col")) {
+        resizeData.newCoords = resizeData.startCoords.x - e.clientX;
+        resizeData.resizeEl.style = `right: ${resizeData.newCoords}px;`;
+      } else {
+        resizeData.newCoords = resizeData.startCoords.y - e.clientY;
+        resizeData.resizeEl.style = `bottom: ${resizeData.newCoords}px;`;
+      }
+    };
+  
+    const mouseUpHandler = () => {
+      if (resizeData) {
+        resizeData.resizeEl.style = ``;
+        if (
+            resizeData.resizeEl.className.includes("col") &&
+            resizeData.newCoords
+        ) {
+          resizeData.elementToRz.style = `width: ${
+              resizeData.elementToRz.offsetWidth - resizeData.newCoords
+          }px`;
+        } else if (
+            !resizeData.resizeEl.className.includes("col") &&
+            resizeData.newCoords
+        ) {
+          resizeData.elementToRz.style = `height: ${
+              resizeData.elementToRz.offsetHeight - resizeData.newCoords
+          }px`;
+        }
+        resizeData = null;
+        instance.componentElement.removeListener("mousemove", mouseMoveHandler);
+        instance.componentElement.removeListener("mouseup", mouseUpHandler);
+      }
+    };
+    instance.componentElement.addListener("mousemove", mouseMoveHandler);
+    instance.componentElement.addListener("mouseup", mouseUpHandler);
   }
 };
 
-const mouseMoveHandler = (instance,e) => {
-  e.preventDefault();
-  if (instance.resize.resizeEl.className.includes("col")) {
-    instance.resize.newCoords = instance.resize.startCoords.x - e.clientX;
-    instance.resize.resizeEl.style = `right: ${instance.resize.newCoords}px;`;
-  } else {
-    instance.resize.newCoords = instance.resize.startCoords.y - e.clientY;
-    instance.resize.resizeEl.style = `bottom: ${instance.resize.newCoords}px;`;
-  }
-};
 
-const mouseUpHandler = (instance) => {
-  if (instance.resize) {
-    instance.resize.resizeEl.style = ``;
-    if (
-      instance.resize.resizeEl.className.includes("col") &&
-      instance.resize.newCoords
-    ) {
-      instance.resize.elementToRz.style = `width: ${
-        instance.resize.elementToRz.offsetWidth - instance.resize.newCoords
-      }px`;
-    } else if (
-      !instance.resize.resizeEl.className.includes("col") &&
-      instance.resize.newCoords
-    ) {
-      instance.resize.elementToRz.style = `height: ${
-        instance.resize.elementToRz.offsetHeight - instance.resize.newCoords
-      }px`;
-    }
-    instance.resize = null;
-    instance.componentElement.removeListener("mousemove", MoveHandler);
-    instance.componentElement.removeListener("mouseup", UpHandler);
-  }
-};
